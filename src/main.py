251158affdd2e99903eb0e4ec9f609ee9efb8738
ROS <Isaac@makerforce.io>
2018-03-19 18:00:27 +0800

    Added some parsing

--- src/main.py
+++ src/main.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 import rospy
 from autoforce_mixer.msg import NodeWeightArray
+import json
+
+results = None
+def callback(data):
+	results = data
 
 def main():
-	pub = rospy.Publisher('autoforce_offb_controller', NodeWeightArray, queue_size=10)
-	ros.init_node('autoforce_offb_controller')
-	rate = rospy.Rate(10)
+	with open('ambrose.json') as data_file:
+		data = json.load(data_file)
+		stages = data["stages"]
+		pub = rospy.Publisher('autoforce_offb_controller', NodeWeightArray, queue_size=10)
+		ros.init_node('autoforce_offb_controller')
+		rate = rospy.Rate(10)
 
-	while rospy.is_shutdown():
-		#something something
-		rate.sleep()
+		current_stage = 0
+		while current_stage < len(stages):
+			stage = stages[current_stage]
+			sub = rospy.Subscribe(stage['node'])
+			while True:
+				if results = None:
+					continue
+				##check ifs
+				rate.sleep()
+			sub.shutdown()
+			rate.sleep()
 
 if __name__ == "__main__":
 	main()
