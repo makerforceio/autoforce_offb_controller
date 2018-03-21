fb3d0355f58e4e122583c6f2f47d601a5a861fca
Isaac Tay <IsaacTay@programmer.net>
2018-03-21 22:23:56 +0800

    Adding some node parsing

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 import rospy
 from autoforce_mixer.msg import NodeWeightArray, NodeWeight
 import json
+from geometry_msgs.msg import Twist
 
 results = None
 def callback(data):
@@ -12,6 +13,7 @@ def main():
 		data = json.load(data_file)
 		stages = data["stages"]
 		pub = rospy.Publisher('autoforce_offb_controller', NodeWeightArray, queue_size=10)
+		pub_constants = rospy.Publisher('autoforce_offb_controller_constants', Twist, queue_size=10)
 		ros.init_node('autoforce_offb_controller')
 		rate = rospy.Rate(10)
 
@@ -22,17 +24,22 @@ def main():
 			out = []
 			nodes = stage["then"]
 			for node in nodes.keys():
+				node_weight = NodeWeight()
 				if node == "twist":
-					
+					node_weight.name = "/autoforce_offb_controller_constants"
+				else:
+					node_weight.name = "/" + node
+				node_weight.weight = nodes[node]
 			while True:
 				if results = None:
 					continue
 				if stage["type"] == "boolean":
 					if results == stage["value"]:
-						pub.
+						pub.publish(NodeWeightArray(out))
 						break
 				if stage["type"] == "range":
 					if stage["min"] < results < stage["max"]:
+						pub.publish(NodeWeightArray(out))
 						break
 				rate.sleep()
 			sub.shutdown()
